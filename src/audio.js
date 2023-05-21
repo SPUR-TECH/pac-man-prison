f4df0adb1aa737701e816a93c2f4cc918db8750b
Scott Atkinson <scott.atkin79@gmail.com>
2023-05-21 19:48:46 +0000

    added audio.js file and howler link for future works

---
+++ src/audio.js
@@ -0,0 +1,31 @@
+// Howler.volume(1)
+// const audio = [{
+
+//     startSound: new Howl({
+//         src: './sounds/start.wav',
+//     }),
+
+//     wakaSound: new Howl({
+//         src: './sounds/waka.wav',
+//     }),
+
+//     powerDotSound: new Howl({
+//         src: './sounds/powerDot.wav',
+//     }),
+
+//     eatGuardSound: new Howl({
+//         src: './sounds/eatGuard.wav',
+//     }),
+
+//     powerDotSound: new Howl({
+//         src: './sounds/powerDot.wav',
+//     }),
+
+//     gameOverSound: new Howl({
+//         src: './sounds/gameOver.wav',
+//     }),
+
+//     gameWinSound: new Howl({
+//         src: './sounds/gameWin.wav',
+//     }),
+// }]
